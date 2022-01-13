22c91a23a77825c3892e770a83ed012fd2df6b72
Miguel Gualito <miguel.gualito@cidesi.edu.mx>
2022-01-13 17:24:55 -0600

    Fixuntracked files

--- Debug/source/errata_SSWF021_45.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-source/errata_SSWF021_45.obj: ../source/errata_SSWF021_45.c
-source/errata_SSWF021_45.obj: C:/Users/tellopa1/Documents/Pablo/Proyectos/PA/CML-CMR/Repositorios/Secuencial/Anexo_Logico_CML_CMR_Sec/include/errata_SSWF021_45_defs.h
-source/errata_SSWF021_45.obj: C:/Users/tellopa1/Documents/Pablo/Proyectos/PA/CML-CMR/Repositorios/Secuencial/Anexo_Logico_CML_CMR_Sec/include/errata_SSWF021_45.h
-
-../source/errata_SSWF021_45.c: 
-C:/Users/tellopa1/Documents/Pablo/Proyectos/PA/CML-CMR/Repositorios/Secuencial/Anexo_Logico_CML_CMR_Sec/include/errata_SSWF021_45_defs.h: 
-C:/Users/tellopa1/Documents/Pablo/Proyectos/PA/CML-CMR/Repositorios/Secuencial/Anexo_Logico_CML_CMR_Sec/include/errata_SSWF021_45.h: 
